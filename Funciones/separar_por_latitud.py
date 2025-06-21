8f8a31d7b73731be974c2ef24c5091b1d9f25fbc
B-Namor <benja_n@MacBook-Pro-de-Benjamin.local>
2025-06-20 20:40:03 -0400

    Función arreglada y comprobada de forma unitaria

--- Funciones/separar_por_latitud.py
+++ Funciones/separar_por_latitud.py
@@ -1,7 +1,10 @@
 def separar_por_latitud(SC_AACGM_LAT, tiempo_final):
     """
-    Separa datos según latitud en rangos específicos y retorna listas ajustadas y comparador.
+    Separa datos según latitud en rangos específicos y retorna listas ajustadas y otras listas.
     """
+    if len(SC_AACGM_LAT) != len(tiempo_final):
+        raise ValueError("Las listas deben tener el mismo largo.")
+
     adjust_SC_AACGM_LAT = []
     other_SC_AACGM_LAT  = []
     adjust_tiempo_final = []
@@ -9,18 +12,19 @@ def separar_por_latitud(SC_AACGM_LAT, tiempo_final):
     comparador = []
     flag = False
 
-    for i, elem in enumerate(SC_AACGM_LAT):
-        if (-75 < elem < -50) or (50 < elem < 75):
-            adjust_SC_AACGM_LAT.append(elem)
-            adjust_tiempo_final.append(tiempo_final[i])
+    for lat_elem, t_elem in zip(SC_AACGM_LAT, tiempo_final):
+        t_elem_stripped = t_elem.strip()
+        if (-75 < lat_elem < -50) or (50 < lat_elem < 75):
+            adjust_SC_AACGM_LAT.append(lat_elem)
+            adjust_tiempo_final.append(t_elem_stripped)
             if not flag:
-                comparador.append((elem, tiempo_final[i]))
-            flag = True
+                comparador.append((lat_elem, t_elem_stripped))
+                flag = True
         else:
             if flag:
-                comparador.append((elem, tiempo_final[i]))
+                comparador.append((lat_elem, t_elem_stripped))
             flag = False
-            other_SC_AACGM_LAT.append(elem)
-            other_tiempo_final.append(tiempo_final[i])
+            other_SC_AACGM_LAT.append(lat_elem)
+            other_tiempo_final.append(t_elem_stripped)
 
     return adjust_SC_AACGM_LAT, adjust_tiempo_final, other_SC_AACGM_LAT, other_tiempo_final, comparador
