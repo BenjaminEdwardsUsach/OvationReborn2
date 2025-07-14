b1115f7d2d577367294595fdfa51d93c62a0d167
BenjaminEdwardsUsach <benjamin.edwards@usach.cl>
2025-07-13 22:48:50 -0400

    Delete funciones directory

--- funciones/agrupar_extremos.py
+++
@@ -1,5 +0,0 @@
-def agrupar_extremos(extremos):
-    """
-    Agrupa extremos en pares para definir segmentos de interés.
-    """
-    return [extremos[i:i + 2] for i in range(0, len(extremos), 2)]
