33e32a03712a18e58ad17593c0a92a551d2d7dab
BenjaminEdwardsUsach <benjamin.edwards@usach.cl>
2025-08-26 01:19:33 -0400

    Create __init__.py

---
+++ funciones/fronteras/__init__.py
@@ -0,0 +1,13 @@
+# funciones/__init__.py
+
+# — Funciones de carga y filtrado —
+from .detect_b1e import detect_b1e
+from .detect_b1i import detect_b1i
+from .detect_b2e import detect_b2e
+from .detect_b2i import detect_b2i
+from .detect_b3a import detect_b3a
+from .detect_b3b import detect_b3b
+from .detect_b4s import detect_b4s
+from .detect_b5e import detect_b5e
+from .detect_b5i import detect_b5i
+from .detect_b6 import detect_b6
