5e9064b23199a60d3cbf4a5b042f5dbe8f081b44
BenjaminEdwardsUsach <benjamin.edwards@usach.cl>
2025-08-26 01:21:18 -0400

    Create __init__.py

---
+++ funciones/fronteras/funciones_auxiliares/__init__.py
@@ -0,0 +1,6 @@
+# funciones/__init__.py
+
+# — Funciones de carga y filtrado —
+from .detect_b5 import detect_b5
+from .has_monoenergetic_peak import has_monoenergetic_peak
+from .is_polar_rain import is_polar_rain
